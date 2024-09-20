b61d76d7e50504045c93f83a3b45e12aaea90beb
Muguntha@17 <mugunthadurairaj@gmail.com>
2024-09-20 17:52:33 -0400

    pattern3

--- src/org/build_logical_thinking/Pattern.java
+++ src/org/build_logical_thinking/Pattern.java
@@ -5,6 +5,7 @@ public class Pattern {
 	public static void main(String[] args) {
 		nForest(5);
 		pattern2(5);
+		pattern3(5);
 
 	}
 //Exercise:1 Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
@@ -26,6 +27,17 @@ public class Pattern {
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<=i;j++) {
 				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+//Exercise:3 Sam is making a Triangular painting for a maths project.
+//An N-dimensional Triangle is represented by the lower triangle of the pattern filled with integers starting from 1.
+//For every value of ‘N’, help sam to print the corresponding N-dimensional Triangle.
+	static void pattern3(int n) {
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print(j+1+" ");
 			}
 			System.out.println();
 		}
