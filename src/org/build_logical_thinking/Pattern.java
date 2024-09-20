57f410a9ec1500e4a25d4c83a30747c3c51e6ab8
Muguntha@17 <mugunthadurairaj@gmail.com>
2024-09-20 17:42:45 -0400

    pattern2

--- src/org/build_logical_thinking/Pattern.java
+++ src/org/build_logical_thinking/Pattern.java
@@ -4,6 +4,7 @@ public class Pattern {
 
 	public static void main(String[] args) {
 		nForest(5);
+		pattern2(5);
 
 	}
 //Exercise:1 Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
@@ -18,4 +19,16 @@ public class Pattern {
 		}
 	}
 
+//Exercise :2 Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
+//An N/2-dimensional forest is represented by the lower triangle of the pattern filled with ‘*’.
+//For every value of ‘N’, help sam to print the corresponding N/2-dimensional forest.
+	static void pattern2(int n) {
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
 }
+
