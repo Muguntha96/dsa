fb3c75ed5278953220de5b328bc8b63b8e6df50b
Muguntha96 <muguntharamkumar1996@gmail.com>
2024-10-04 17:14:11 -0400

    pattern9

--- src/org/build_logical_thinking/Pattern.java
+++ src/org/build_logical_thinking/Pattern.java
@@ -12,6 +12,7 @@ public class Pattern {
 		pattern7(4);
 		pattern8(4);
 		pattern9(4);
+		
 	}
 //Exercise:1 Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
 
@@ -142,7 +143,7 @@ Input Format: N = 6 Result:
 				System.out.print(" ");
 			}
 			for(int j=0;j<2*n-(2*i+1);j++) {
-				System.out.print(j);
+				System.out.print("*");
 			}
 			for(int j=1;j<=i;j++) {
 				System.out.print(" ");
@@ -150,19 +151,44 @@ Input Format: N = 6 Result:
 			System.out.println();
 		}
 	}
-	static void pattern9(int n) {
+
+/* 	Exercise:9
+	Input Format: N = 3
+Result: 
+  *  
+ ***
+***** 
+*****  
+ ***
+  * */   
+	static void pattern9(int n) {	
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n-i-1;j++) {
+				System.out.print(" ");
+			}
+			for(int j=0;j<2*i+1;j++) {
+				System.out.print("*");
+			}
+			for(int j=0;j<n-i-1;j++) {
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	
 		for(int i=0;i<n;i++) {
 			for(int j=1;j<=i;j++) {
 				System.out.print(" ");
 			}
 			for(int j=0;j<2*n-(2*i+1);j++) {
-				System.out.print(j);
+				System.out.print("*");
 			}
 			for(int j=1;j<=i;j++) {
 				System.out.print(" ");
 			}
 			System.out.println();
 		}
+		
+
 	}
 }
 
