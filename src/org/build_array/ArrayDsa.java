6862c5ea8f8fb479e9bc4282be77ce2d629559c8
Muguntha96 <muguntharamkumar1996@gmail.com>
2024-10-15 23:03:29 -0400

    reverse string

--- src/org/build_array/ArrayDsa.java
+++ src/org/build_array/ArrayDsa.java
@@ -6,7 +6,9 @@ public class ArrayDsa {
 
 	public static void main(String[] args) {
 		int[] arr= {1,2,3,4};
-		System.out.println(Arrays.toString(addingEle(arr)));;
+		System.out.println(Arrays.toString(addingEle(arr)));
+		String s= "Geeks";
+		System.out.println(reverseString(s));
 
 	}
 	
@@ -25,5 +27,20 @@ public class ArrayDsa {
 		return newArr;
 		
 	}
+	//Reverse Strings
+	 public static String reverseString(String s) {
+//		StringBuilder sb=new StringBuilder(s);
+//		sb.reverse();
+		char[] sArr=s.toCharArray();
+		for(int i=0;i<sArr.length/2;i++) {
+			char temp=sArr[i];
+			sArr[i]=sArr[sArr.length-i-1];
+			sArr[sArr.length-i-1]=temp;
+		}
+		
+		String newString=new String(sArr);
+		return newString;
+	       
+	    }
 
 }
